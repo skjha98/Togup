b08b10417bf0c65c7ba380d76848cd0cbbaa19ac
shivi96 <shivigupta2628@gmail.com>
2018-08-18 15:06:52 +0530

    shivani icon added login , logout working, toast working, sign up working proudfully done!!!

--- app/src/main/java/com/sgsj/togup/MainActivity.java
+++ app/src/main/java/com/sgsj/togup/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.auth.FirebaseAuth;
@@ -33,6 +34,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.drawer_menu_withdraw:
                 getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, new AboutFragment()).commit();
                 break;
+            case R.id.drawer_menu_log_out:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(this,"You have been Logout",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,AuthLoginActivity.class));
         }
         mainDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
